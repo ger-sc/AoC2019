c3f3da9f11eda9d33f498c9720ebdbd08386cbe9
schuster.gerald <schuster.gerald@fronius.com>
2019-12-02 13:31:46 +0100

    day2

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace day2
 {
@@ -6,7 +7,61 @@ namespace day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = System.IO.File.ReadAllLines("input.txt");
+            var parsedInput = input[0].Split(',').Select(x => int.Parse(x)).ToArray();
+
+            var index = 0;
+            
+            parsedInput[1] = 12;
+            parsedInput[2] = 2;
+            while(parsedInput[index] != 99) {
+                var command = parsedInput[index];
+                var input1 = parsedInput[index+1];
+                var input2 = parsedInput[index+2];
+                var output = parsedInput[index+3];
+                switch(command) {
+                    case 1:
+                      parsedInput[output] = parsedInput[input1] + parsedInput[input2];
+                      index += 4;
+                      break;
+                    case 2:
+                      parsedInput[output] = parsedInput[input1] * parsedInput[input2];
+                      index += 4;
+                      break;
+                }
+            }
+            Console.Out.WriteLine(parsedInput[0]);
+            
+            for(var noun = 0; noun < 100; noun++) {
+                
+                for(var verb = 0; verb < 100; verb++) {
+                    
+                    var parsedInput2 = input[0].Split(',').Select(x => int.Parse(x)).ToArray();
+                    parsedInput2[1] = noun;
+                    parsedInput2[2] = verb;
+                    index = 0;
+                    while(parsedInput2[index] != 99) {
+                        var command = parsedInput2[index];
+                        var input1 = parsedInput2[index+1];
+                        var input2 = parsedInput2[index+2];
+                        var output = parsedInput2[index+3];
+                        switch(command) {
+                            case 1:
+                            parsedInput2[output] = parsedInput2[input1] + parsedInput2[input2];
+                            index += 4;
+                            break;
+                            case 2:
+                            parsedInput2[output] = parsedInput2[input1] * parsedInput2[input2];
+                            index += 4;
+                            break;
+                        }
+                    }
+                    if (parsedInput2[0] == 19690720) {
+                        Console.Out.WriteLine(100*noun+verb);
+                    }
+                }
+            }
+
         }
     }
 }
