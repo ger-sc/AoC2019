f00f530fede1fc105790bdf4a1fbf6f2ff1820bd
Gerald Schuster <ger.schuster@gmail.com>
2019-12-09 19:08:54 +0100

    day09

---
+++ day09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt").Split(",").Select(x => int.Parse(x));
+            var amp = new Amplifier(input.ToArray(), t => {Console.WriteLine(t);});
+            amp._queue.Enqueue(1);
+            amp.Runner();
+            var amp2 = new Amplifier(input.ToArray(), t => {Console.WriteLine(t);});
+            amp2._queue.Enqueue(2);
+            amp2.Runner();
+        }
+    }
+}
