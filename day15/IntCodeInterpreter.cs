adebd3d9130c290f69af37e63abfc4368810c28f
Gerald Schuster <ger.schuster@gmail.com>
2019-12-15 17:40:09 +0100

    day15

---
+++ day15/IntCodeInterpreter.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Numerics;
+
+
+public class IntCodeInterpreter
+{
+
+
+  private enum Mode : int
+  {
+    Position = 0,
+    Immediate = 1,
+    Relative = 2
+  }
+
+  private IDictionary<BigInteger, BigInteger> OpCode = new Dictionary<BigInteger, BigInteger>();
+  private Action<BigInteger> Output;
+  public ConcurrentQueue<BigInteger> _queue = new ConcurrentQueue<BigInteger>();
+
+  private BigInteger RelativeBase = 0;
+
+
+  public IntCodeInterpreter(BigInteger[] opCode, Action<BigInteger> output)
+  {
+    for (var i = 0; i < opCode.Length; i++)
+    {
+      OpCode[i] = opCode[i];
+    };
+    Output = output;
+  }
+
+  private BigInteger GetValueAt(BigInteger index)
+  {
+    if (!OpCode.ContainsKey(index))
+    {
+      OpCode[index] = 0;
+    }
+    return OpCode[index];
+  }
+
+  private BigInteger GetValue(Mode mode, BigInteger input)
+  {
+    switch (mode)
+    {
+      case Mode.Position:
+        return GetValueAt(input);
+      case Mode.Immediate:
+        return input;
+      case Mode.Relative:
+        return GetValueAt(input + RelativeBase);
+      default:
+        throw new ArgumentException("Unknown mode", nameof(mode));
+    }
+  }
+
+  private BigInteger GetAddress(Mode mode, BigInteger input)
+  {
+    switch (mode)
+    {
+      case Mode.Position:
+        return input;
+      case Mode.Relative:
+        return RelativeBase + input;
+      default:
+        throw new ArgumentException("Unknown mode", nameof(mode));
+    }
+  }
+
+  public void Runner()
+  {
+    BigInteger index = 0;
+    while (OpCode[index] != 99)
+    {
+      var instr = GetValueAt(index);
+      var input1 = GetValueAt(index + 1);
+      var input2 = GetValueAt(index + 2);
+      var input3 = GetValueAt(index + 3);
+      var command = (int)instr % 100;
+      var modes = ((int)instr / 100).ToString().PadLeft(3, '0').ToCharArray().Reverse().ToArray();
+
+      var mode1 = int.Parse(modes[0].ToString());
+      var mode2 = int.Parse(modes[1].ToString());
+      var mode3 = int.Parse(modes[2].ToString());
+      BigInteger value1, value2;
+
+      switch (command)
+      {
+        case 1:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          OpCode[GetAddress((Mode)mode3, input3)] = value1 + value2;
+          index += 4;
+          break;
+        case 2:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          OpCode[GetAddress((Mode)mode3, input3)] = value1 * value2;
+          index += 4;
+          break;
+        case 3:
+          BigInteger msg;
+          while (!_queue.TryDequeue(out msg))
+          {
+            Thread.Sleep(TimeSpan.FromMilliseconds(1));
+          }
+          OpCode[GetAddress((Mode)mode1, input1)] = msg;
+          index += 2;
+          break;
+        case 4:
+          value1 = GetValue((Mode)mode1, input1);
+          Output(value1);
+          index += 2;
+          break;
+        case 5:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          if (value1 > 0)
+          {
+            index = value2;
+          }
+          else
+          {
+            index += 3;
+          }
+          break;
+        case 6:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          if (value1 == 0)
+          {
+            index = value2;
+          }
+          else
+          {
+            index += 3;
+          }
+          break;
+        case 7:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          if (value1 < value2)
+          {
+            OpCode[GetAddress((Mode)mode3, input3)] = 1;
+          }
+          else
+          {
+            OpCode[GetAddress((Mode)mode3, input3)] = 0;
+          }
+          index += 4;
+          break;
+        case 8:
+          value1 = GetValue((Mode)mode1, input1);
+          value2 = GetValue((Mode)mode2, input2);
+          if (value1 == value2)
+          {
+            OpCode[GetAddress((Mode)mode3, input3)] = 1;
+          }
+          else
+          {
+            OpCode[GetAddress((Mode)mode3, input3)] = 0;
+          }
+          index += 4;
+          break;
+        case 9:
+          value1 = GetValue((Mode)mode1, input1);
+          RelativeBase += value1;
+          index += 2;
+          break;
+      }
+    }
+  }
+}
+
+
---
+++ day15/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Numerics;
+using System.Drawing;
+using System.Threading;
+
+namespace day15 {
+  public static class DictExtensions {
+    public static string GetOrDefault(this IDictionary<Point, string> dict, Point key) {
+      return dict.ContainsKey(key) ? dict[key] : " ";
+    }
+  }
+
+  internal enum Direction {
+    North = 1,
+    South = 2,
+    West = 3,
+    East = 4
+  }
+
+  internal static class Program {
+    private static readonly ConcurrentQueue<BigInteger> Output = new ConcurrentQueue<BigInteger>();
+    private static readonly IDictionary<Point, string> Map = new Dictionary<Point, string>();
+
+    private static readonly IDictionary<Point, int> Distances = new Dictionary<Point, int>();
+
+    public static void Main() {
+      var input = System.IO.File.ReadAllText("input.txt").Split(",").Select(BigInteger.Parse).ToArray();
+      var droid = new Point(0, 0);
+      var source = new Point(-1, -1);
+      var dir = Direction.North;
+      var code = new IntCodeInterpreter(input, msg => Output.Enqueue(msg));
+      var t = new Thread(code.Runner);
+      Map.Add(droid, ".");
+      Distances.Add(droid, 0);
+      t.Start();
+      while (t.IsAlive && droid != source) {
+        source = new Point(0, 0);
+        DrawMap(droid);
+        code._queue.Enqueue((int) dir);
+        BigInteger msg;
+        while (!Output.TryDequeue(out msg)) {
+          Thread.Sleep(TimeSpan.FromMilliseconds(1));
+        }
+
+        Point newPosition;
+        switch ((int) msg) {
+          case 0:
+            var wall = GetPosition(droid, dir);
+            if (!Map.ContainsKey(wall)) {
+              Map.Add(wall, "█");
+            }
+
+            dir = TurnRight(dir);
+            break;
+          case 1:
+            newPosition = GetPosition(droid, dir);
+
+            if (Distances.TryGetValue(newPosition, out var newDistance)) {
+              Distances[newPosition] = Math.Min(newDistance, Distances[droid] + 1);
+            }
+            else {
+              Distances.Add(newPosition, Distances[droid] + 1);
+            }
+
+            if (!Map.ContainsKey(newPosition)) {
+              Map.Add(newPosition, ".");
+            }
+
+            droid = newPosition;
+            var left = GetLeftField(droid, dir);
+            if (left != "█") {
+              dir = TurnLeft(dir);
+            }
+
+            break;
+          case 2:
+            newPosition = GetPosition(droid, dir);
+            if (Distances.TryGetValue(newPosition, out var newDistance2)) {
+              Distances[newPosition] = Math.Min(newDistance2, Distances[droid] + 1);
+            }
+            else {
+              Distances.Add(newPosition, Distances[droid] + 1);
+            }
+
+            if (!Map.ContainsKey(newPosition)) {
+              Map.Add(newPosition, "2");
+            }
+
+            droid = newPosition;
+            break;
+        }
+      }
+
+      var oxygenPos = Map.Single(x => x.Value == "2").Key;
+      Console.Out.WriteLine(Distances[oxygenPos]);
+
+      Map[oxygenPos] = "O";
+      var timer = 0;
+      do {
+        var oxygen = Map.Where(x => x.Value == "O").ToDictionary(x => x.Key, x => x.Value);
+        foreach (var oxy in oxygen.Keys) {
+          var space = GetNoWalls(oxy);
+          foreach (var s in space) {
+            Map[s] = "O";
+          }
+        }
+
+        DrawMap(new Point(0, 0));
+        timer++;
+      } while (Map.Any(x => x.Value == "."));
+
+      Console.Out.WriteLine(timer);
+    }
+
+    private static IEnumerable<Point> GetNoWalls(Point p) {
+      if (Map.GetOrDefault(new Point(p.X - 1, p.Y)) == ".") {
+        yield return new Point(p.X - 1, p.Y);
+      }
+
+      if (Map.GetOrDefault(new Point(p.X + 1, p.Y)) == ".") {
+        yield return new Point(p.X + 1, p.Y);
+      }
+
+      if (Map.GetOrDefault(new Point(p.X, p.Y - 1)) == ".") {
+        yield return new Point(p.X, p.Y - 1);
+      }
+
+      if (Map.GetOrDefault(new Point(p.X, p.Y + 1)) == ".") {
+        yield return new Point(p.X, p.Y + 1);
+      }
+    }
+
+    private static string GetLeftField(Point droid, Direction dir) {
+      switch (dir) {
+        case Direction.North:
+          return Map.GetOrDefault(new Point(droid.X - 1, droid.Y));
+        case Direction.South:
+          return Map.GetOrDefault(new Point(droid.X + 1, droid.Y));
+        case Direction.West:
+          return Map.GetOrDefault(new Point(droid.X, droid.Y + 1));
+        case Direction.East:
+          return Map.GetOrDefault(new Point(droid.X, droid.Y - 1));
+        default:
+          throw new ArgumentException("Unknown direction", nameof(dir));
+      }
+    }
+
+    private static Point GetPosition(Point start, Direction dir) {
+      switch (dir) {
+        case Direction.North:
+          return new Point(start.X, start.Y - 1);
+        case Direction.South:
+          return new Point(start.X, start.Y + 1);
+        case Direction.West:
+          return new Point(start.X - 1, start.Y);
+        case Direction.East:
+          return new Point(start.X + 1, start.Y);
+        default:
+          throw new ArgumentException("Unknown direction", nameof(dir));
+      }
+    }
+
+    private static void DrawMap(Point droid) {
+      Console.Out.WriteLine();
+      var minX = Map.Select(x => x.Key.X).Min();
+      var maxX = Map.Select(x => x.Key.X).Max();
+      var minY = Map.Select(x => x.Key.Y).Min();
+      var maxY = Map.Select(x => x.Key.Y).Max();
+      for (var y = minY; y <= maxY; y++) {
+        for (var x = minX; x <= maxX; x++) {
+          var p = new Point(x, y);
+          if (p == droid) {
+            Console.Out.Write("D");
+          }
+          else {
+            Console.Out.Write(Map.ContainsKey(p) ? Map[p] : " ");
+          }
+        }
+
+        Console.Out.WriteLine();
+      }
+
+      Console.Out.WriteLine();
+    }
+
+    private static Direction TurnRight(Direction dir) {
+      switch (dir) {
+        case Direction.North:
+          return Direction.East;
+        case Direction.South:
+          return Direction.West;
+        case Direction.West:
+          return Direction.North;
+        case Direction.East:
+          return Direction.South;
+        default:
+          throw new ArgumentException("Unknown direction", nameof(dir));
+      }
+    }
+
+    private static Direction TurnLeft(Direction dir) {
+      switch (dir) {
+        case Direction.North:
+          return Direction.West;
+        case Direction.South:
+          return Direction.East;
+        case Direction.West:
+          return Direction.South;
+        case Direction.East:
+          return Direction.North;
+        default:
+          throw new ArgumentException("Unknown direction", nameof(dir));
+      }
+    }
+  }
+}
