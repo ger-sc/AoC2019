7ff45a5d84e7cf8b75f754304ef091c24dfd29c9
schuster.gerald <schuster.gerald@fronius.com>
2019-12-16 08:51:35 +0100

    day16 part 1

---
+++ day16/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day16
+{
+    class Program
+    {
+        private static int[] Pattern = new int[] {0,1,0,-1};
+        private static IEnumerable<int> GeneratePattern(int level) {
+            bool first = true;
+            while(true) {
+                for (var i = 0; i < Pattern.Length; i++) {
+                    for(var r = 0; r < level; r++) {
+                        if(first) {
+                            first = false;
+                        } else {
+                            yield return Pattern[i];
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt").ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+
+            //var input = new int[] {1,2,3,4,5,6,7,8};
+            var output = new int[input.Length];
+            var step = 0;
+            while (step < 100) {
+                for(var i = 0; i < input.Length; i++) {
+                    var p = GeneratePattern(i+1).Take(input.Length).ToArray();
+                    var sum = 0;
+                    for(var z = 0; z < input.Length; z++) {
+                        sum+= input[z]*p[z];
+                    }
+                    output[i] = Math.Abs(sum % 10);
+                } 
+                input = output;
+                step++;
+            }
+
+            Console.Out.WriteLine(string.Join("", output.Take(8).Select(x => x.ToString())));
+            
+            var inputString = System.IO.File.ReadAllText("input.txt");
+            input = inputString.ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+            var inputLength = inputString.Length * 10000;
+            var realInput = GenerateRealInput(input);
+            var offset = int.Parse(inputString.Substring(0, 7));
+            output = new int[inputLength];
+            step = 0;
+            while (step < 100) {
+                for(var i = 0; i < inputLength; i++) {
+                    var p = GeneratePattern(i+1);
+                    var sum = 0;
+                    var iEnum = realInput.GetEnumerator();
+                    var pEnum = p.GetEnumerator();
+                    var iterator = 0;
+                    while(iEnum.MoveNext()) {
+                        pEnum.MoveNext();
+                        sum += iEnum.Current * pEnum.Current;
+                        iterator++;
+                    }
+                    output[i] = Math.Abs(sum % 10);
+                } 
+                realInput = output.AsEnumerable();
+                step++;
+            }
+            Console.Out.WriteLine(string.Join("", output.Skip(offset).Take(8).Select(x => x.ToString())));
+        }
+
+        private static IEnumerable<int> GenerateRealInput(int[] input)
+        {
+            for(var i = 0; i < 10000; i++) {
+                for(var x = 0; x < input.Length; x++) {
+                    yield return input[x];
+                }
+            }
+        }
+    }
+}
